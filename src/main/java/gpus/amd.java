42b0ee8b618be4f405b5d9898ef6ca9d9bd967a2
olkorch <alexkor@qamadness.com>
2023-06-28 15:40:06 +0300

    array+loop
    added array and loop

--- src/main/java/gpus/amd.java
+++ src/main/java/gpus/amd.java
@@ -1,4 +1,16 @@
 package gpus;
 
+
 public class amd {
+    String model = "rx6600";
+    int ram = 8;
+    public String getModel() {
+        return model;
+        }
+    public int getRam() {
+        return ram;
+    }
+    public void gpuInfo() {
+        System.out.println("model" + model + "ram" + ram);
+    }
 }
--- src/main/java/gpus/intel.java
+++ src/main/java/gpus/intel.java
@@ -1,4 +1,15 @@
 package gpus;
 
 public class intel {
+    String model = "arc770";
+    int ram = 8;
+    public String getModel() {
+        return model;
+    }
+    public int getRam() {
+        return ram;
+    }
+    public void gpuInfo() {
+        System.out.println("model" + model + "ram" + ram);
+    }
 }
